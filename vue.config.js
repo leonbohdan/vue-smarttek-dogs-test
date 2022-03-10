a96afb2d733fc9f062ae06991294f05e93f9674b
Bohdan Leonets <leonetsbogdan@gmail.com>
2022-03-10 09:34:38 +0200

    deployment settings were added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-smarttek-dogs-test/" : "/",
 });
